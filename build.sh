e65170bafc5c1a832ac8081f785bc6380dd5c040
Ankit Dhanawat <115169073+jainankit0811@users.noreply.github.com>
2025-03-29 12:30:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID ankitd002/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID ankitd002/$JOB_NAME:latest
 
-docker push vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker push ankitd002/$JOB_NAME:$BUILD_ID
 
-docker push vaibhavjoshi26/$JOB_NAME:latest
+docker push ankitd002/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID ankitd002/$JOB_NAME:$BUILD_ID ankitd002/$JOB_NAME:latest
